08fc083884b58349583838ea9afdd60374b57fb2
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2019-02-06 23:03:45 -0600

    web displays about and date html pages

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('',views.AboutPageView.as_view()),
+    path('date/',views.DatePageView.as_view()),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+class DatePageView(TemplateView):
+    def get(self,request,**kwargs):
+        return render(request,'date.html',context=None)
+
+class AboutPageView(TemplateView):
+   template_name = "about.html"
