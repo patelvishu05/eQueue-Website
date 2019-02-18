55c46c584546bc93144b5779abbcc2a155faf3c4
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2019-02-18 01:08:26 -0600

    web displays about and date html pages

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,23 @@ from django.urls import path
 from django.conf.urls import url
 
 from . import views
+from django.urls import path
+from . import views
 
+# urlpatterns = [
+#     # path('', views.index, name='index'),
+#     path('',views.AboutPageView.as_view()),
+#     path('date/',views.DatePageView.as_view()),
+# ]
+app_name = 'polls'
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('',views.AboutPageView.as_view()),
-    path('date/',views.DatePageView.as_view()),
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
